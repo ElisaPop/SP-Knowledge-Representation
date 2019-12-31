0b06156379f71d68aee7ae8f6f80da91b57c6643
Pop Elisa <ElisaPop@users.noreply.github.com>
2019-12-31 18:35:13 +0200

    First question can be asked The dictionary doesn't update afterwards

--- game.py
+++ game.py
@@ -1,20 +1,63 @@
 import query_operations as qo
 
+item = ()
+seek = 0
+results = 0
 
-def askQuestion():
-    ordered_dictionary = qo.get_entropies()
-    item = next(reversed(ordered_dictionary))
+hasFlower = 0
+hasFruit = 0
 
-    print(ordered_dictionary.items())
+def ask_question():
+    global item
 
-    print("Is the plant a " + ordered_dictionary.get(item).get('name') + "?")
-    answer = str(input())
-    if (answer == 'Y'):
-        qo.append_exists(item)
+    if results == 0:
+        for sub in ordered_dictionary:
+            print(" " + ordered_dictionary[sub]['name'] + ": " + str(ordered_dictionary[sub]['entropy']))
     else:
-        qo.append_not_exists(item)
+        for sub in ordered_dictionary:
+            print(" " + ordered_dictionary[sub]['name'])
+
+    item = ordered_dictionary.popitem(True)
+    if item[1]['name'] == qo.current_label:
+        item = ordered_dictionary.popitem(True)
+
+    print("Is the plant a " + item[1]['name'] + "?")
+    # print(type(item))
 
 
 if __name__ == "__main__":
+
+    print("Is the plant a Flower?")
+    answer = str(input())
+    if answer == 'Y':
+        hasFlower = 1
+    elif answer == 'N':
+        hasFlower = 0
+
+    qo.get_initial_max()
+
+
+
     while True:
-        askQuestion()
+        if seek == 0 and results == 0:
+            ordered_dictionary = qo.get_entropies()
+            if not bool(ordered_dictionary):
+                results = 1
+
+        if seek == 0 and results == 1:
+            ordered_dictionary = qo.get_results()
+
+        if not ordered_dictionary.keys():
+            break
+        else:
+            ask_question()
+            answer = str(input())
+            if answer == 'Y':
+                seek = 0
+                qo.append_exists(item[0])
+                ordered_dictionary.clear()
+            elif answer == 'N':
+                seek = 0
+                qo.append_not_exists(item[0])
+            else:
+                seek = 1
--- query_operations.py
+++ query_operations.py
@@ -10,9 +10,10 @@ sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 # global string_subclass_part_2
 # global string_subclass_part_3
 
-string_subclass_part_1 = """SELECT DISTINCT ?org WHERE { ?org rdfs:subClassOf yago:Plant100017222 """
-string_subclass_part_2 = """ . MINUS { SELECT DISTINCT ?org WHERE {{?org rdfs:subClassOf yago:Performer110415638"""
-string_subclass_part_3 = ". }}}}"
+string_subclass_part_1 = """SELECT DISTINCT ?org WHERE { ?org rdfs:subClassOf yago:"""
+string_subclass_part_2 = """Plant100017222"""
+string_subclass_part_3 = " . }"
+
 #
 # global string_type_part_1
 # global string_type_part_2
@@ -20,9 +21,15 @@ string_subclass_part_3 = ". }}}}"
 
 string_type_part_1 = """ WHERE { ?tag rdf:type yago:Plant100017222"""
 string_type_part_2 = """ . MINUS { SELECT DISTINCT ?tag WHERE {{?tag rdf:type: yago:WikicatAmericanFilms"""
-string_type_part_3 = ". }}}}"
+string_type_part_3 = " . }}}}"
+
+current_label = "Plant"
+max_results = 27983
 
 entropy_dictionary = dict()
+results_dictionary = dict()
+sub_class_list = list
+not_sub_class_list = list
 
 
 def concat_string_types():
@@ -33,71 +40,82 @@ def concat_string_subclass():
     return string_subclass_part_1 + string_subclass_part_2 + string_subclass_part_3
 
 
-def append_exists(subClass):
+def append_exists(sub_class):
     global string_type_part_1
-    global string_subclass_part_1
-
-    string_subclass_part_1 += "; rdfs:subClassOf yago:" + subClass
-    string_type_part_1 += """; rdf:type yago:""" + subClass
-
-    #print(concat_string_subclass())
-    #print(concat_string_types())
-
-
-def append_not_exists(subClass):
-    global string_type_part_2
     global string_subclass_part_2
+    global current_label
 
-    string_subclass_part_2 += "; rdfs:subClassOf yago:" + subClass
-    string_type_part_2 += """}UNION {?tag rdf:type yago:""" + subClass
+    current_label = ''.join(i for i in sub_class if not i.isdigit())
 
-    #print(concat_string_subclass())
-    #print(concat_string_types())
+    string_subclass_part_2 = sub_class
+    string_type_part_1 += """; rdf:type yago:""" + sub_class
+
+
+def append_not_exists(sub_class):
+    global string_type_part_2
+    string_type_part_2 += """}UNION {?tag rdf:type yago:""" + sub_class
 
 
 def run_query():
-    sparql.setQuery(string_subclass_part_1 + string_subclass_part_2 + string_subclass_part_3)
+    sparql.setQuery(concat_string_subclass())
     sparql.setReturnFormat(JSON)
     results = sparql.query()
 
 
-###WORKING ###TESTED
 def nr_results(sub_class):
     sparql.setQuery(
-        """SELECT (count(distinct ?tag)) as ?org""" + string_type_part_1 + """; rdf:type yago:""" + sub_class + string_type_part_2 + string_type_part_3)
+        """SELECT (count(distinct ?tag)) as ?org""" + string_type_part_1 + """; rdf:type yago:""" + sub_class
+        + string_type_part_2 + string_type_part_3)
     sparql.setReturnFormat(JSON)
     results1 = sparql.query().convert()
     results_string = str(results1)
-    # print("""SELECT (count(distinct ?tag)) as ?org""" + string_type_part_1 + """ rdf:type yago:""" + subClass  + string_type_part_2 + string_type_part_3)
+    # print("""SELECT (count(distinct ?tag)) as ?org""" + string_type_part_1 + """; rdf:type yago:""" + sub_class
+    #     + string_type_part_2 + string_type_part_3)
+    # print(results_string[results_string.find("'value': '") + 10: results_string.find("'}}]")])
     return results_string[results_string.find("'value': '") + 10: results_string.find("'}}]")]
 
 
-###WORKING ###TESTED
-def nr_non_results(subClass):
+def nr_non_results(sub_class):
     sparql.setQuery(
-        """SELECT (count(distinct ?tag)) as ?org""" + string_type_part_1 + string_type_part_2 + """}UNION {?tag rdf:type yago:""" + subClass + string_type_part_3)
+        """SELECT (count(distinct ?tag)) as ?org""" + string_type_part_1 + string_type_part_2 +
+        """}UNION {?tag rdf:type yago:""" + sub_class + string_type_part_3)
     sparql.setReturnFormat(JSON)
     results1 = sparql.query().convert()
     results_string = str(results1)
-    # print("""SELECT (count(distinct ?tag)) as ?org""" + string_type_part_1 + string_type_part_2 + """}UNION {?tag rdf:type yago:""" + subClass  + string_type_part_3)
+    # print("""SELECT (count(distinct ?tag)) as ?org""" + string_type_part_1 + string_type_part_2 +
+    #          """}UNION {?tag rdf:type yago:""" + sub_class + string_type_part_3)
+    # print(results_string[results_string.find("'value': '") + 10: results_string.find("'}}]")])
     return results_string[results_string.find("'value': '") + 10: results_string.find("'}}]")]
+
+
+def get_initial_max():
+    global max_results
+    sparql.setQuery("""SELECT (count(distinct ?tag)) as ?org""" + concat_string_types())
+    sparql.setReturnFormat(JSON)
+    results1 = sparql.query().convert()
+    results_string = str(results1)
+    max_results = int(results_string[results_string.find("'value': '") + 10: results_string.find("'}}]")])
+    print(max_results)
 
 
 def weird_division(n, d):
+    if n + d > max_results: return 0
     return n / d if d else 0
 
 
 def get_entropy(sub_class):
     nr_res = int(nr_results(sub_class))
     nr_non_res = int(nr_non_results(sub_class))
-    if nr_res != 0 or nr_non_res != 0:
+    div = weird_division(nr_res, nr_non_res)
+    if div != 0:
         import re
         new_string = re.sub(r"(\w)([A-Z])", r"\1 \2", sub_class)
         new_string = re.sub(r'[0-9]+', '', new_string)
         new_string = new_string.replace('Wikicat ', '')
         new_string = new_string.replace('Plants', 'Plant')
+        new_string = new_string.replace('Trees', 'Tree')
         entropy_dictionary[sub_class] = {'name': new_string, 'number': nr_res,
-                                         'entropy': weird_division(nr_res, nr_non_res)}
+                                         'entropy': div}
 
 
 def set_entropies():
@@ -106,15 +124,42 @@ def set_entropies():
     sparql.setReturnFormat(JSON)
     results = sparql.query()
     for result in results:
-        resultString = str(result)
-        startPosition = resultString.find("yago/")
-        endPosition = resultString.find("\" }")
-        if startPosition != -1 and resultString.find("DescribedIn") is -1 and resultString.find(
-                '&') is -1 and resultString[startPosition + 5: endPosition].find('\\') is -1:
-            get_entropy(resultString[startPosition + 5: endPosition])
+        result_string = str(result)
+        start_position = result_string.find("yago/")
+        end_position = result_string.find("\" }")
+        if start_position != -1 and result_string.find("DescribedIn") is -1 and result_string.find(
+                '&') is -1 and result_string[start_position + 5: end_position].find('\\') is -1 \
+                and result_string[start_position + 5: end_position].find('Of') is -1 \
+                and result_string[start_position + 5: end_position].find('As') is -1 \
+                and result_string[start_position + 5: end_position].find('Plant By') is -1 \
+                and result_string[start_position + 5: end_position].find('Plant In') is -1 \
+                and result_string[start_position + 5: end_position].find('.') is -1 \
+                and result_string[start_position + 5: end_position].find('(.)') is -1:
+            get_entropy(result_string[start_position + 5: end_position])
+
+
+def set_results():
+    sparql.setQuery("""SELECT DISTINCT ?tag""" + concat_string_types())
+    print("""SELECT DISTINCT ?tag""" + concat_string_types())
+    sparql.setReturnFormat(JSON)
+    results = sparql.query()
+    for result in results:
+        result_string = str(result)
+        print(result_string)
+        start_position = result_string.find("resource/")
+        end_position = result_string.find("\" }")
+        if start_position != -1 and result_string.find("DescribedIn") is -1 and result_string.find(
+                '&') is -1 and result_string[start_position + 9: end_position].find('\\') is -1 \
+                and result_string[start_position + 9: end_position].find('Of') is -1 \
+                and result_string[start_position + 9: end_position].find('As') is -1 \
+                and result_string[start_position + 9: end_position].find('.') is -1 \
+                and result_string[start_position + 9: end_position].find('(.)') is -1:
+            results_dictionary[result_string[start_position + 9: end_position]] = {
+                'name': result_string[start_position + 9: end_position]}
 
 
 def print_entropies():
+    entropy_dictionary.clear()
     set_entropies()
     res = OrderedDict(sorted(entropy_dictionary.items(),
                              key=lambda x: (getitem(x[1], 'entropy'), getitem(x[1], 'number'))))
@@ -123,12 +168,23 @@ def print_entropies():
 
 
 def get_entropies():
+    entropy_dictionary.clear()
     set_entropies()
     return OrderedDict(sorted(entropy_dictionary.items(),
                               key=lambda x: (getitem(x[1], 'entropy'), getitem(x[1], 'number'))))
 
-if __name__ == "__main__":
-    print(nr_non_results("PoisonousPlant113100156"))
-    print(nr_results("PoisonousPlant113100156"))
 
+def get_results():
+    results_dictionary.clear()
+    set_results()
+    return OrderedDict(sorted(results_dictionary.items(),
+                              key=lambda x: (getitem(x[1], 'name'))))
+
+
+if __name__ == "__main__":
+    # print(nr_non_results("PoinsonousPlants"))
+    # print(nr_results("PoinsonousPlants"))
+    # get_initial_max()
+    print_entropies()
+    append_exists("Tree113104059")
     print_entropies()
